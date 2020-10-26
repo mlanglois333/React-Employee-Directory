260ccaa8030b4786781b9a65010e9daa6b8815ac
Marc <mlanglois333@icloud.com>
2020-10-26 09:49:20 -0400

    Component Buildout

---
+++ employeedirectory/src/components/EmployeeInfo.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import EmployeeInfo from "./EmployeeInfo";
+import API from "../API";
