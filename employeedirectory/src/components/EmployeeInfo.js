89e43612e8273e75d50a8cf1ba54b1412bb0bba9
Marc <mlanglois333@icloud.com>
2020-10-28 13:59:49 -0400

    Debug

--- employeedirectory/src/components/EmployeeInfo.js
+++ employeedirectory/src/components/EmployeeInfo.js
@@ -4,23 +4,30 @@ import Container from "./Container";
 import Row from "./Row";
 import Col from "./Col";
 import SearchResults from "./SearchResults";
+import OptionsForm from "./OptionsForm"
 
 class EmployeeInfo extends Component {
 
     state = {
         isLoading: true,
-        result: undefined
+        result: undefined,
+        filter: "",
+        sort: "",
+        dataToRender: undefined,
+
+        
     };
 
     componentDidMount() {
         axios.get("https://randomuser.me/api/?results=20&nat=us")
-            .then(res => this.setState({ result: res.data.results, isLoading: false }))
+            .then(res => this.setState({ result: res.data.results, isLoading:false }))
             .catch(err => console.log(err));
 
     };
 
     render() {
-        const { isLoading, result } = this.state;
+        const { isLoading, result} = this.state;
+
         if (isLoading) {
             return <div>
                 <h3>
@@ -29,11 +36,15 @@ class EmployeeInfo extends Component {
             </div>
         }
         else {
+            //const male = result.filter(result.gender === "male");
+           // const female = result.filter(result.gender === "female");
+            //const ascend = result.name.last.sort();
+           // const descend = ascend.reverse();
             return <div>
                 <Container>
                     <Row>
                         <Col size="md-12">
-                            <h1 class="jumbotron">
+                            <h1 className="jumbotron">
                                 Employee Roster
                             </h1>
                         </Col>
@@ -42,6 +53,9 @@ class EmployeeInfo extends Component {
                         <Col size="md-8">
                             <SearchResults results={result} />
 
+                        </Col>
+                        <Col size="md-4">
+                            <OptionsForm />
                         </Col>
                     </Row>
 
--- employeedirectory/src/components/OptionsForm.js
+++ employeedirectory/src/components/OptionsForm.js
@@ -4,24 +4,30 @@ function OptionsForm(props) {
     return (
         <div>
             <h3>Filter Options</h3>
+            <br></br>
             <form>
-                <label for="gender">Gender:</label>
+                <label> Gender:</label>
+
+                <br></br>
 
                 <select name="gender" id="gender">
                     <option value="none">No Filter</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                 </select>
+                <br></br>
 
-                <label for="alphabetize">Alphabetical</label>
+                <label>Alphabetical</label>
+                <br></br>
 
                 <select name="gender" id="gender">
                     <option value="none">No Filter</option>
                     <option value="ascend">A-Z</option>
                     <option value="descend">Z-A</option>
                 </select>
+                <br></br>
 
-                <input type="submit" value ="submit">Submit</input>
+                <button type="submit" value ="submit">Submit</button>
             </form>
 
 
